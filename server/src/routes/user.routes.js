9d7357a39a2b1d0b463bf2e688df032f7433240e
kummancoder <kummandhakad@gmail.com>
2025-01-05 22:48:35 +0530

    add logout functionality to user controller and route

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controllers/user.controllers.js";
+import { login, logout, register } from "../controllers/user.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-
+router.route("/logout").post(verifyJWT,logout);
 
 export default router;
