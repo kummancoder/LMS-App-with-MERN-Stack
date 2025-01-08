124b631a57ed165f680230e5f57739c626c91e72
kummancoder <kummandhakad@gmail.com>
2025-01-08 20:45:43 +0530

    HeroSection component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginSignUp from './pages/LoginSignUp'; 
 import Navbar from './components/Navbar';
+import HeroSection from './pages/student/HeroSection';
 
 const App = () => {
   return (
     <main>
       <Navbar/>
+      <HeroSection/>
       <LoginSignUp /> 
     </main>
   );
