a2098c22bc67d74bcc40c4feeca2ca3e92a6b49f
kummancoder <kummandhakad@gmail.com>
2025-01-08 21:12:30 +0530

    add react-router-dom for routing; implement MainLayout and update App For routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import LoginSignUp from './pages/LoginSignUp'; 
-import Navbar from './components/Navbar';
-import HeroSection from './pages/student/HeroSection';
+import React from "react";
+import LoginSignUp from "./pages/LoginSignUp";
+import Navbar from "./components/Navbar";
+import HeroSection from "./pages/student/HeroSection";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from "./layout/MainLayout";
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <HeroSection />
+            {/* Courses */}
+          </>
+        ),
+      },
+      {
+        path: "/login",
+        element: <LoginSignUp />,
+      },
+    ],
+  },
+]);
 const App = () => {
   return (
     <main>
-      <Navbar/>
-      <HeroSection/>
-      <LoginSignUp /> 
+      <RouterProvider router = {appRouter}/>
     </main>
   );
 };
