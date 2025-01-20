09287fb6059f8da72ddecff8eaa9fb29b5c971b9
kummancoder <kummandhakad@gmail.com>
2025-01-20 23:53:59 +0530

    refactor user model; simplify schema by removing unnecessary fields and updating naming conventions; enhance password handling and add photoUrl field

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,80 +1,38 @@
-import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const userSchema = new Schema(
-  {
-    fullName: {
-      type: String,
-      required: true,
-      trim: true,
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
     email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowecase: true,
-      trim: true,
+        type: String,
+        required: true
     },
     password: {
-      type: String,
-      required: [true, "Password is required"],
-      minlength: 8,
+        type: String,
+        required: true
     },
     role: {
-      type: String,
-      enum: ["instructor", "student"],
-      default: "student",
+        type: String,
+        enum:['student','instructor'],
+        default: 'student'
     },
-    enrolledCourses: [{
-      type: Schema.Types.ObjectId,
-      ref: "Course",
-    }],
-    avatar: {
-      type: String,
-      default: "",
-    },
-    refreshToken: {
-      type: String,
-    },
-  },
-  { timestamps: true }
-);
-
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
+    enrolledCourses: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Course'
+        }
+    ],
+    photoUrl: {
+        type: String,
+        default: ""
+    }
+},{timestamps: true})
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
+    return await bcrypt.compare(password, this.password);  
+}
 
-userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      email: this.email,
-      fullName: this.fullName,
-      role: this.role,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-    }
-  );
-};
-userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    }
-  );
-};
 export const User = mongoose.model("User", userSchema);
